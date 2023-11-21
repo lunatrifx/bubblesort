df84c883d816c036488b8c88f82f5f1705a8ca81
Paul Dominick Lavigne <97206814+lunatrifx@users.noreply.github.com>
2023-11-21 12:34:11 -0800

    Changed for function

--- bubblesort.swift
+++ bubblesort.swift
@@ -2,7 +2,7 @@ func bubbleSort (_ array: inout [int]) -> [int] {
     let n = array.count // this function seeks to get the length of the entire array
     var swapped = false // this variable is used to check if the array is sorted or not
     while true {
-        for i in 0 ..<n {
+        for i in 0 ... n-1 {
             if array[i] > array[i+1] {
                 let temp = array[i] // here we are swapping the values using a temporary variable
                 array[i] = array[i+1]
@@ -14,8 +14,6 @@ func bubbleSort (_ array: inout [int]) -> [int] {
         if swapped == false { // if the array is not swapped, then it is sorted
             break
         }
-        return array
-    } {
-        
     }
+    return array 
 }
